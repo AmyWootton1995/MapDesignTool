03de4e948d69ba81a724b43f278adcf89429ba68
AmyWootton1995 <30521932+AmyWootton1995@users.noreply.github.com>
2017-09-08 16:09:15 +0200

    Add orientation and incorrect kriging

--- Scripts/Map Design Tool.py
+++ Scripts/Map Design Tool.py
@@ -2,17 +2,21 @@
 # Name:        Map Design Tool
 # Purpose:     BSc (Hons) Geoinformatics Project
 #
-# Author:      Amy Wootton
+# Author:      Amy Wootton, tferreira
 #
 # Created:     24/08/2017
+
 # Copyright:   (c) Amy Wootton 2017
 # Licence:     University of Pretoria
 #-------------------------------------------------------------------------------
-import os,sys,arcpy, pythonaddins
+import os,sys,arcpy, pythonaddins, datetime, arcgisscripting, string
 from arcpy import env
 
+i = datetime.datetime.now()
+MyDate = "%s%s_%s%s%s_" % (i.hour, i.minute, i.day, i.month, i.year) 
+
 #   --Set Parameters--
-# Path name of input text file
+# Path name of input csv file
 ParmInTxt = arcpy.GetParameterAsText(0) 
 # Name of map title
 ParmMapName = arcpy.GetParameterAsText(1)
@@ -22,10 +26,16 @@ ParmPointsName = arcpy.GetParameterAsText(2)
 ParmBaseMapInfo = arcpy.GetParameterAsText(3)
 #Projection
 ParmProjection = arcpy.GetParameterAsText(4)
+#Interpolation
+ParmInterpolation = arcpy.GetParameterAsText(5)
+#Styling
+ParmStyling = arcpy.GetParameterAsText(6)
 # Credit Info
-ParmCredits = arcpy.GetParameterAsText(5)
+ParmCredits = arcpy.GetParameterAsText(7)
 # Name of Saved Layer
-ParmSaveLayerName = arcpy.GetParameterAsText(6) + ".lyr"
+ParmSaveLayerName = MyDate + arcpy.GetParameterAsText(8) + ".lyr"
+
+arcpy.env.overwriteOutput =  True
 
 #   --
 arcpy.AddMessage("Designing your map has begun...")
@@ -43,23 +53,28 @@ try:
     df = arcpy.mapping.ListDataFrames(mxd,"Layers")[0]
 
     #   --Defining types of basemaps--
-    arcpy.AddMessage("Adding in Base Map...")
     if ParmBaseMapInfo == "Open Street Map":
-        BaseMap = "OSM.lyr"
+        BaseMap = "BaseMap3 (DO NOT DELETE).lyr"
     elif ParmBaseMapInfo == "Topographic Map":
-        BaseMap = "Topo.lyr"
+        BaseMap = "BaseMap4 (DO NOT DELETE).lyr"
     elif ParmBaseMapInfo == "Aerial Image (without labels)":
-        BaseMap = "AWOL.lyr"
+        BaseMap = "BaseMap1 (DO NOT DELETE).lyr"
     elif ParmBaseMapInfo == "National Geopgraphic":
-        BaseMap = "NatGeo.lyr"
+        BaseMap = "BaseMap2 (DO NOT DELETE).lyr"
+    elif ParmBaseMapInfo == "None":
+    	BaseMap = "None"
     else:
-        BaseMap = "OSM.lyr"
-        pythonaddins.MessageBox("Open Street Map has been used as a default base map, because the selected base map could not be found!", "Welcome to Map Design Tool", 0)
+        BaseMap = "None"
+        pythonaddins.MessageBox("No base map has been selected", "Welcome to Map Design Tool", 0)
     
     #   --Adding in basemap from root folder--
-    path_to_layer = os.path.join(os.path.dirname(os.path.abspath(__file__)), BaseMap)
-    addBase = arcpy.mapping.Layer(path_to_layer)
-    arcpy.mapping.AddLayer(df, addBase, "BOTTOM")
+    if BaseMap == "None":
+    	arcpy.AddMessage("No Base Map Selected...")
+    else:
+    	arcpy.AddMessage("Adding in Base Map...")
+    	path_to_layer = os.path.join(os.path.dirname(os.path.abspath(__file__)), BaseMap)
+    	addBase = arcpy.mapping.Layer(path_to_layer)
+    	arcpy.mapping.AddLayer(df, addBase, "BOTTOM")
 
     #   --Save layer into root folder--
     saved_Layer = os.path.join(os.path.dirname(os.path.abspath(__file__)), ParmSaveLayerName)
@@ -129,8 +144,68 @@ try:
     elemTitle.text = ParmMapName
 
     #   --Changing Credits Layout Element--
-    elemCredits = arcpy.mapping.ListLayoutElements(mxd, "TEXT_ELEMENT", "Credits")[0]
-    elemCredits = ParmCredits 
+    elemCredits = arcpy.mapping.ListLayoutElements(mxd, "TEXT_ELEMENT", "credits")[0]
+    elemCredits.text = "Credits: " + ParmCredits + "  Date: %s/%s/%s" % (i.day, i.month, i.year)
+
+    #	--Getting last row name in csv--
+    featureclass = addPoints
+    field_names = [f.name for f in arcpy.ListFields(featureclass)]
+    count = len(field_names)
+    QuantifyName = field_names[count - 2]
+    arcpy.AddMessage(QuantifyName) 
+
+    #	--Interpolation--
+
+    if ParmInterpolation == "Yes":
+    	gp = arcgisscripting.create() #geoprocessor object create
+    	# Check out any necessary licenses
+    	gp.checkoutextension("Spatial")
+    	# Set the input feature dataset
+    	inputFeatureDataset = addPoints
+    	attributeName = QuantifyName
+    	# Set the output raster name
+    	outputRaster = "C:/data/final_1"
+    	# Define the semivariogram
+    	Model = "Spherical"         #(Remember the space at the end)
+    	LagSize = "0.4496 "          #(Remember the space at the end)
+    	MajorRange = "2.6185 "       #(Remember the space at the end)
+    	PartialSill = "542.65 "      #(Remember the space at the end)
+    	Nugget = "0"
+    	Semivariogram = LagSize + MajorRange + PartialSill + Nugget
+    	# Don't write out the variance raster
+    	Output_variance_of_prediction_raster = ""
+    	gp.Kriging_sa(inputFeatureDataset, attributeName, outputRaster, Model, Semivariogram, "1", "VARIABLE 12", Output_variance_of_prediction_raster)
+    	arcpy.AddMessage("Completed IDW")
+
+    	# Method 1
+    	#arcpy.InterpolateFromPointCloud_management(addPoints, 'C:/data/dsm.crf', '10','IDW', 'GAUSS5x5', 'DSM')
+
+    	# Method 2
+    	# Inter = "Yes"
+    	# inPoints = addPoints
+    	# inField = QuantifyName
+    	# outRaster = 'outImgServ'
+    	# optimizeFor = 'SPEED'
+    	# transform = 'False'
+    	# subsetSize = 50
+    	# numNeighbors = 8
+    	# outCellSize = '10000 Meters'
+    	# error = 'NO_OUTPUT_ERROR'
+    	# arcpy.InterpolatePoints_ra(inPoints, inField, outRaster, optimizeFor, transform, subsetSize, numNeighbors, outCellSize, error)
+
+    elif ParmInterpolation == "No":
+    	Inter = "No"
+    else:
+    	arcpy.AddMessage("Interpolation failed")
+
+
+    #	--Styling--
+    if ParmStyling == "Yes":
+    	Style = "Yes"
+    elif ParmStyling == "No":
+    	Style = "No"
+    else:
+    	arcpy.AddMessage("Styling failed")
 
     #   --Change to Layout view--
     arcpy.mapping.MapDocument("current").activeView = "PAGE_LAYOUT"
